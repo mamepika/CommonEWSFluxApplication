d3309261422bccaaed151e8a0a922056cf14d5da
mamepika <starlightprayer@gmail.com>
2021-02-04 22:11:59 +0900

    add json parse

--- src/main/java/jp/ac/aiit/pbl/smz/commonewsfluxapplication/api/ServerSentCommonEWSController.java
+++ src/main/java/jp/ac/aiit/pbl/smz/commonewsfluxapplication/api/ServerSentCommonEWSController.java
@@ -2,6 +2,7 @@ package jp.ac.aiit.pbl.smz.commonewsfluxapplication.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
@@ -19,8 +20,9 @@ public class ServerSentCommonEWSController {
 
     @GetMapping(path = "/feed",
         produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<CommonEWSSeptember2020> feed(){
+    public Flux<CommonEWSSeptember2020> feed(final Model model){
         final Flux<CommonEWSSeptember2020> commonStream = Flux.fromIterable(this.messageRepository.findLatestOne()).delayElements(Duration.ofSeconds(5L));
+        model.addAttribute("data",commonStream);
         return commonStream;
     }
 }
